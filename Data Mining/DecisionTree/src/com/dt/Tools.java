4add9b3bc1c3c9dc9c6592a65d6e5178c3d9a24f
heyufeng <foreverfruit@126.com>
2017-10-29 21:16:10 +0800

    java path blank problem

--- Data Mining/DecisionTree/src/com/dt/Tools.java
+++ Data Mining/DecisionTree/src/com/dt/Tools.java
@@ -4,15 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Tools {
 
 	/**
 	 * 加载项目data目录下的data文件，自动分发到两个数据集
 	 * @throws IOException 
+	 * @throws URISyntaxException 
 	 */
-	public static void loadTxt() throws IOException {
-		String path = Tools.class.getResource("/").getPath();
+	public static void loadTxt() throws IOException, URISyntaxException {
+		String path = Tools.class.getResource("/").toURI().getPath();
 		File file = new File(path,"iris.data");
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String temp;
--- Data Mining/DecisionTree/src/com/test/TestApp.java
+++ Data Mining/DecisionTree/src/com/test/TestApp.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import org.junit.Test;
 
@@ -9,13 +10,13 @@ import com.dt.Tools;
 public class TestApp {
 
 	@Test
-	public void testPath() {
-		String path = Tools.class.getResource("/").getPath();
+	public void testPath() throws URISyntaxException {
+		String path = Tools.class.getResource("/").toURI().getPath();
 		System.out.println(path);
 	}
 	
 	@Test
-	public void testLoadTxt() throws IOException {
+	public void testLoadTxt() throws IOException, URISyntaxException {
 		Tools.loadTxt();
 	}
 }
